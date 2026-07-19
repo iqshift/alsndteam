ca55d731040ca30be8b47004a820bea39366f1ea
iqshift <your@email.com>
2026-07-19 13:09:17 +0300

    feat: configure apps to use HTTPS secure connection with teamsanad.com

--- pos_app/lib/core/config/constants.dart
+++ pos_app/lib/core/config/constants.dart
@@ -1,7 +1,5 @@
 class AppConstants {
-  // غير عنوان الـ IP هنا فقط ليتم تحديثه في كامل التطبيق تلقائياً
-  static const String serverIp = '169.58.32.137';
-  static const String serverPort = '3000';
+  static const String domain = 'teamsanad.com';
 
-  static const String baseUrl = 'http://$serverIp:$serverPort/api';
+  static const String baseUrl = 'https://$domain/api';
 }
--- restaurant-app/lib/core/config/constants.dart
+++ restaurant-app/lib/core/config/constants.dart
@@ -1,9 +1,6 @@
 class AppConstants {
-  // غير عنوان الـ IP هنا فقط ليتم تحديثه في كامل التطبيق تلقائياً
-  // 192.168.1.103 هو IP الحاسبة المحلي في الشبكة ليصل إليه الهاتف الحقيقي والمحاكي
-  static const String serverIp = '192.168.1.103';
-  static const String serverPort = '3000';
+  static const String domain = 'teamsanad.com';
 
-  static const String baseUrl = 'http://$serverIp:$serverPort/api';
-  static const String socketUrl = 'http://$serverIp:$serverPort';
+  static const String baseUrl = 'https://$domain/api';
+  static const String socketUrl = 'https://$domain';
 }
